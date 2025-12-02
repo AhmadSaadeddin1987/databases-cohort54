03e7480492a6c2dcb35d2e1206da0b0f545fd231
Ahmad Saadeddin <A.saadeddin87@gmail.com>
2025-12-02 14:12:52 +0100

    modify-print-command

--- Week2/Joins.js
+++ Week2/Joins.js
@@ -14,7 +14,7 @@ console.table(res1.rows);
 
 console.log("\nAuthors and their paper titles:");
 let res2 = await client.query(`
-    SELECT a.author_name, rp.paper_title
+    SELECT a.*, rp.paper_title
     FROM authors a
     LEFT JOIN author_papers ap ON a.author_id = ap.author_id
     LEFT JOIN research_papers rp ON ap.paper_id = rp.paper_id;
