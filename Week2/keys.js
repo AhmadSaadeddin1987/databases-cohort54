3447bedcf53af7fda6592f37dc238bbe49c9772e
Ahmad Saadeddin <A.saadeddin87@gmail.com>
2025-12-02 13:43:59 +0100

    modify-date-value

--- Week2/keys.js
+++ Week2/keys.js
@@ -15,8 +15,8 @@ await client.query(`
     CREATE TABLE authors (
     author_id SERIAL PRIMARY KEY,
     author_name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
     university VARCHAR(100),
-    date_of_birth DATE,
     h_index INT,
     gender VARCHAR(20)
     );
